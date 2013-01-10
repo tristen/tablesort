6da83802dbb7bba51942ad073ea7bff52529d84f
tristen <tristen@tristen.ca>
2013-01-09 21:14:45 -0500

    make uncompressed script more implicit

--- grunt.js
+++ grunt.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
     concat: {
       dist: {
         src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
-        dest: '<%= pkg.name %>.js'
+        dest: '<%= pkg.name %>.uncompressed.js'
       }
     },
     min: {
