9f33b1541bbedfdcd1c7034b31443d455be650b0
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2015-07-03 18:28:40 +0200

    Use single quotes

--- src/sorts/tablesort.monthname.js
+++ src/sorts/tablesort.monthname.js
@@ -5,7 +5,7 @@
       item.search(/(January|February|March|April|May|June|July|August|September|October|November|December)/i) !== -1
     );
   }, function(a, b) {
-    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     return monthNames.indexOf(b) - monthNames.indexOf(a);
   });
 }());
