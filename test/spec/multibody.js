f87bd5ab12ed8996b16fe6dfad14bffb4ebf7d2f
RogerDodger <roger.dodger.thor@gmail.com>
2016-03-14 17:56:18 +1030

    add test that tbodys aren't moved

--- test/spec/multibody.js
+++ test/spec/multibody.js
@@ -7,9 +7,9 @@ tape('sorts tbodys independently', function(t) {
   t.equal(tableMulti.rows[1].cells[0].innerHTML, '1');
   t.equal(tableMulti.rows[2].cells[0].innerHTML, '2');
   t.equal(tableMulti.rows[3].cells[0].innerHTML, '3');
-  t.equal(tableMulti.rows[4].cells[0].innerHTML, '1');
-  t.equal(tableMulti.rows[5].cells[0].innerHTML, '2');
-  t.equal(tableMulti.rows[6].cells[0].innerHTML, '3');
+  t.equal(tableMulti.rows[4].cells[0].innerHTML, '2');
+  t.equal(tableMulti.rows[5].cells[0].innerHTML, '3');
+  t.equal(tableMulti.rows[6].cells[0].innerHTML, '4');
 
   event.initEvent('click', true, false);
   el.dispatchEvent(event);
@@ -17,9 +17,16 @@ tape('sorts tbodys independently', function(t) {
   t.equal(tableMulti.rows[1].cells[0].innerHTML, '3');
   t.equal(tableMulti.rows[2].cells[0].innerHTML, '2');
   t.equal(tableMulti.rows[3].cells[0].innerHTML, '1');
-  t.equal(tableMulti.rows[4].cells[0].innerHTML, '3');
-  t.equal(tableMulti.rows[5].cells[0].innerHTML, '2');
-  t.equal(tableMulti.rows[6].cells[0].innerHTML, '1');
+  t.equal(tableMulti.rows[4].cells[0].innerHTML, '4');
+  t.equal(tableMulti.rows[5].cells[0].innerHTML, '3');
+  t.equal(tableMulti.rows[6].cells[0].innerHTML, '2');
+
+  t.equal(tableMulti.tBodies[0].rows[0].cells[0].innerHTML, '3');
+  t.equal(tableMulti.tBodies[0].rows[1].cells[0].innerHTML, '2');
+  t.equal(tableMulti.tBodies[0].rows[2].cells[0].innerHTML, '1');
+  t.equal(tableMulti.tBodies[1].rows[0].cells[0].innerHTML, '4');
+  t.equal(tableMulti.tBodies[1].rows[1].cells[0].innerHTML, '3');
+  t.equal(tableMulti.tBodies[1].rows[2].cells[0].innerHTML, '2');
 
   t.end();
 });
